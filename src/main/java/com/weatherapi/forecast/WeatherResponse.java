09aa348ebb469c2ce1b9f3326e54e52999e70a92
TRON-SV <jerson_trn_232@hotmail.com>
2023-03-30 16:34:06 -0600

    First Commit; Just Added the WeatherResponse class which will handle the data coming from the request to the weatherapi

---
+++ src/main/java/com/weatherapi/forecast/WeatherResponse.java
@@ -0,0 +1,79 @@
+package com.weatherapi.forecast;
+
+import com.google.gson.annotations.SerializedName;
+
+public class WeatherResponse {
+    @SerializedName("forecast")
+    private Forecast forecast;
+
+    public Forecast getForecast() {
+        return forecast;
+    }
+
+    public void setForecast(Forecast forecast) {
+        this.forecast = forecast;
+    }
+
+    public static class Forecast {
+        @SerializedName("forecastday")
+        private ForecastDay[] forecastDays;
+
+        public ForecastDay[] getForecastDays() {
+            return forecastDays;
+        }
+
+        public void setForecastDays(ForecastDay[] forecastDays) {
+            this.forecastDays = forecastDays;
+        }
+
+        public static class ForecastDay {
+            @SerializedName("date")
+            private String date;
+
+            @SerializedName("day")
+            private Day day;
+
+            public String getDate() {
+                return date;
+            }
+
+            public void setDate(String date) {
+                this.date = date;
+            }
+
+            public Day getDay() {
+                return day;
+            }
+
+            public void setDay(Day day) {
+                this.day = day;
+            }
+
+            public static class Day {
+                @SerializedName("condition")
+                private Condition condition;
+
+                public Condition getCondition() {
+                    return condition;
+                }
+
+                public void setCondition(Condition condition) {
+                    this.condition = condition;
+                }
+
+                public static class Condition {
+                    @SerializedName("text")
+                    private String text;
+
+                    public String getText() {
+                        return text;
+                    }
+
+                    public void setText(String text) {
+                        this.text = text;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/test/java/com/weatherapi/forecast/ForecastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weatherapi.forecast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForecastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
